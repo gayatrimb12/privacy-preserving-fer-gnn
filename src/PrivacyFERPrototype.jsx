5b80360f0cec07a618f09fe6d06c4d7d7cb80c04
Gayatri Mylarapu Bagepalli <gayatri.mylarapu@gmail.com>
2026-04-20 22:52:53 -0400

    Refactor FaceLandmarker import to use window object

--- src/PrivacyFERPrototype.jsx
+++ src/PrivacyFERPrototype.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import { FaceLandmarker, FilesetResolver } from "@mediapipe/tasks-vision";
+const { FaceLandmarker, FilesetResolver } = window;
 
 const MODEL_ASSET_PATH =
   "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task";
